03f3fc667484f717d769050e3b6fd09a036d2f7c
emanuel-20 <79278121+emanuel-20@users.noreply.github.com>
2021-06-06 16:35:38 -0600

    Emoji.cs

---
+++ Emoji.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotExamenFinal.Clases.Bots
+{
+    public static class Emoji
+    {
+        public const string Clapper = "🎬";
+        public const string X = "❌";
+        public const string Page_Facing_Up = "📄";
+        public const string Currency_Exchange = "💱";
+    }
+}
